cad7b0e9ba698bcf2f639a114e4a09e110a0ecb1
David <david.montejano@hotmail.com>
2024-02-15 23:22:48 +0100

    t

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,29 @@
 package org.example;
 
 public class Main {
+
+    static MetodosBD m = new MetodosBD();
     public static void main(String[] args) {
 
-    MetodosBD m = new MetodosBD();
+        //En el main llamaremos a todos los metodos de la clase MetodosBD agregados anteriormenete para comprobar su funcionalidad
 
+        System.out.println("Insertar");
+        m.InsertarDocumento();
 
-    m.InsertarDocumento();
-    m.visualizarDocumento();
+        System.out.println("Visualizar el documento");
+        m.visualizarDocumento();
+
+        System.out.println("Voy a pasar los datos a un fichero");
+        m.MongoDBToTextFile();
+
+        System.out.println("Contar registros");
+        m.contarRegidtros();
+
+        System.out.println("Campo eliminado");
+        m.BorrarUnRegistro();
+
+        System.out.println("Campo actualizado");
+        m.ActualizarUnRegistro();
 
     }
 }
--- src/main/java/org/example/MetodosBD.java
+++ src/main/java/org/example/MetodosBD.java
@@ -1,30 +1,42 @@
 package org.example;
 
 import com.mongodb.client.*;
-import com.mongodb.client.internal.MongoClientImpl;
+import com.mongodb.client.result.DeleteResult;
+import com.mongodb.client.result.UpdateResult;
 import org.bson.Document;
+import org.bson.types.ObjectId;
 
+import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Date;
 
 public class MetodosBD {
 
 
-    private MongoDatabase database;
-    private MongoCollection<Document> collection = null;
+    private static MongoClient mongoClient;
+    private static MongoDatabase database;
+    private static MongoCollection<Document> collection;
+
+    //Lo primero definimos la conexion estatica para no repetir constantemente la apertura de la conexion y no saturar el sistema
+
+    static {
+        try {
+
+                mongoClient = MongoClients.create("mongodb://localhost:27017");
+                database = mongoClient.getDatabase("bbddPrueba");
+                collection = database.getCollection("coleccionPrueba");
+                System.out.println("Conexión establecida correctamente");
+
+        } catch (Exception e) {
+            // Manejar cualquier excepción que pueda ocurrir durante la inserción
+            e.printStackTrace();
+        }
+    }
 
 
-
+    //El primer metodo sirve para insertar en la base de datos contenido
     public void InsertarDocumento(){
-
-
-        try (MongoClient mongoclient = MongoClients.create("mongodb://localhost:27017")) {
-
-            database = mongoclient.getDatabase("bbddPrueba");
-            collection = database.getCollection("coleccionPrueba");
-
-            System.out.println("Conexión establecida correctamente");
-
 
             // Verificar si la colección está configurada
             if (collection != null) {
@@ -36,49 +48,41 @@ public class MetodosBD {
             } else {
                 System.out.println("La colección no está configurada. La conexión a la base de datos puede haber fallado.");
             }
-        } catch (Exception e) {
-            // Manejar cualquier excepción que pueda ocurrir durante la inserción
-            e.printStackTrace();
-        }
 
 
     }
 
+    //El siguiente metodo sirve para visualizar el contenido de la base de datos.
+
         public void visualizarDocumento() {
 
-            try (MongoClient mongoclient = MongoClients.create("mongodb://localhost:27017")) {
-
-                database = mongoclient.getDatabase("bbddPrueba");
-                collection = database.getCollection("coleccionPrueba");
-
-                System.out.println("Conexión establecida correctamente");
+        //Con este objeto definimos la colleccion con el metodo find que en mongoDB sirve para imprimir todo el contenido de dicha collecion
 
             FindIterable<Document> documents = collection.find();
 
+            //Una vez el objeto documents tenga todos los datos sacaremos por pantalla con un for each todos los datos
             for(Document doc : documents){
                 System.out.println(doc.toJson());
             }
 
-        }catch (Exception e){
-            e.getStackTrace();
-        }
     }
+
+    //Este metodo sirve para meter el contenido de la base de datos en un txt en la ubicacion definida en formato JSON
 
     public void MongoDBToTextFile(){
 
-        try (MongoClient mongoclient = MongoClients.create("mongodb://localhost:27017")) {
 
-            database = mongoclient.getDatabase("bbddPrueba");
-            collection = database.getCollection("coleccionPrueba");
-
-            System.out.println("Conexión establecida correctamente");
-
-
+        //Definimos un cursor el cual se hara cargo de mover todos los datos de la bbdd
             try (MongoCursor<Document> cursor = collection.find().iterator()){
 
-                String file = "datos_exportados.txt";
+                //Definimos el fichero donde deseamos contener esta base de datos
+                File file = new File("datos_exportados.txt");
+
+                //Definimos un objeto de FileWrite pare escribir en el archivo
 
                 try(FileWriter writer = new FileWriter(file)){
+
+                    //Y mediante un bucle while metemos todo el contemido en el fichero
 
                     while(cursor.hasNext()){
 
@@ -90,37 +94,54 @@ public class MetodosBD {
                         System.out.println("Datos exportados correctamente");
 
                     }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
                 }
             }
-        }catch (Exception e){
-            e.getStackTrace();
-        }
     }
 
-/*
-    private void conexion(){
+    //Este metodo sirve para hacer recuento de todos los registros de la bbdd
+    public void contarRegidtros(){
 
-        try (MongoClient mongoclient = MongoClients.create("mongodb://localhost:27017")) {
+            //Puedes agregar tus filtros aquí
+            Document filtros = new Document();
 
-        database = mongoclient.getDatabase("bbddPrueba");
-        collection = database.getCollection("coleccionPrueba");
+            //Con esta sentencia meto en una variable todos los registros para poder imprimirlos
+            long numRegistros = collection.countDocuments(filtros);
+            System.out.println("Número de registros encontrados: " + numRegistros);
 
-        System.out.println("Conexión establecida correctamente");
 
-        }catch (Exception e){
-            e.getStackTrace();
-            System.out.println("Error al establecer la conexión: " + e.getMessage());
-        }
 
     }
 
-    public void cerrarConexion() {
-        if (mongoClient != null) {
-            mongoClient.close();
-            System.out.println("Conexión cerrada correctamente");
-        }
-    }*/
+    //Este metodo borra un registro
+    public void BorrarUnRegistro(){
 
+            //Aqui agregas el id que quieres borrar
+            Document filtros = new Document("_id","65ce5d55d7b98116416ee70e");
 
+            //Y con el metodo DeleteOne lo eliminas
+            DeleteResult eliminado = collection.deleteOne(filtros);
+            System.out.println("Registro eliminado: " + eliminado.getDeletedCount());
+
+    }
+
+    //Este metodo actualiza un registro
+    public void ActualizarUnRegistro(){
+
+            //Definimos dos variables del tipo document en el cual la variable id es la clave del registro que queremos ahcer update
+            Document id = new Document("_id",new ObjectId("65ce619ee5aa6229b0410a5a"));
+
+            //En esta variable definiremos que paremetro queremos actualizar
+            Document setUpdate = new Document("$set",new Document("Fecha","3"));
+
+            //Lo añadimos a una variable del tipo UpdateResult
+            UpdateResult update = collection.updateOne(id,setUpdate);
+
+            System.out.println("Campo actualizado correctamente: " + update.getModifiedCount());
+
+            //Despues haremos uso del metodo creado anteriormente para visualizar de nuevo la BBDD
+            visualizarDocumento();
+    }
 
 }
